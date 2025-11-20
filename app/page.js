7838af164991f233021e56a284c4cf3376d9dd55
aditehh <aditya.aryal888@gmail.com>
2025-11-20 13:10:47 +0545

    initial vommit

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-    <div>
-      prismaaaaa
-    </div>
+      <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
+        <h1 className="text-3xl font-semibold">
+          Welcome to my blog
+        </h1>
+        <Link href={"/posts"} className="underline">
+          View posts
+        </Link>
+      </main>
     </>
   );
 }
---
+++ app/posts/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <main className='flex flex-col items-center gap-y-5 pt-24 text-center'>
+            <h1 className='text-3xl font-semibold'>
+                All posts (0)
+            </h1>
+            <ul className='border-t border-b border-black/10 py-5 leading-8'>
+
+            </ul>
+        </main>
+    )
+}
+
+export default page
